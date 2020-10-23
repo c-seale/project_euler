36c94e6101f8f84b3d3b9e2e744ecb530700109e
c-seale <codyseale@gmail.com>
2020-10-23 19:59:39 -0400

    p5 solution

---
+++ solutions/5.py
@@ -0,0 +1,23 @@
+from sys import maxsize
+
+"""
+    Project Euler: Largest palindrome product (Problem 4)
+
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+"""
+
+DIVISIBLE_RANGE = (1, 20)
+MAX_SEARCH = maxsize
+
+for i in range(1, MAX_SEARCH + 1):
+    divisible = True
+    for divider in range(DIVISIBLE_RANGE[0], DIVISIBLE_RANGE[1] + 1):
+        if i % divider != 0:
+            divisible = False
+            break
+
+    if divisible:
+        print(i)
+        break
