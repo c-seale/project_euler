5c8b4e09b7f72191fcde57187db776c09a022c4f
c-seale <codyseale@gmail.com>
2020-10-23 22:46:30 -0400

    Update p3
    Make is_prime(...) execute faster

--- solutions/3.py
+++ solutions/3.py
@@ -17,7 +17,7 @@ def is_prime(value: int) -> bool:
     val = abs(value)
     if val in [0, 1]:
         return False
-    for potential_factor in range(2, val // 2 + 1):
+    for potential_factor in range(2, int(sqrt(val)) + 1):
         if val % potential_factor == 0:
             return False
     return True
