f6c5a720078394008423e40cd310de0cf50095c6
c-seale <codyseale@gmail.com>
2020-10-23 22:46:40 -0400

    p7 solution

---
+++ solutions/7.py
@@ -0,0 +1,35 @@
+from math import sqrt
+from sys import maxsize
+
+"""
+    Project Euler: Sum square difference (Problem 6)
+    https://projecteuler.net/problem=6
+
+
+    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    What is the 10 001st prime number?
+"""
+
+TARGET_PRIME_INDEX = 10001
+
+
+def is_prime(value: int) -> bool:
+    val = abs(value)
+    if val in [0, 1]:
+        return False
+    for potential_factor in range(2, int(sqrt(val)) + 1):
+        if val % potential_factor == 0:
+            return False
+    return True
+
+
+def get_prime_by_index(target_index: int) -> int:
+    primes = []
+    for i in range(1, maxsize):
+        if len(primes) == target_index:
+            return primes[-1]
+        if is_prime(i):
+            primes.append(i)
+
+
+print(get_prime_by_index(TARGET_PRIME_INDEX))
